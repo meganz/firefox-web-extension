0d2a043f951619be6361491e825866b37484ef4d
Diego Casorran <diegocr@users.noreply.github.com>
2018-04-25 21:30:33 +0200

    Handle drop links.

--- mega.js
+++ mega.js
@@ -37,6 +37,9 @@ chrome.webRequest.onBeforeRequest.addListener(function(details) {
                 if (details.url.indexOf('https://mega.nz/embed') > -1) {
                     hash = '#E' + hash.substr(1);
                 }
+                if (details.url.indexOf('https://mega.nz/drop') > -1) {
+                    hash = '#D' + hash.substr(1);
+                }
             }
             else if (details.url.indexOf('https://mega.nz/') > -1 && details.url.length > 16) {
                 hash = '#' + details.url.split('https://mega.nz/')[1];
